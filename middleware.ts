dd09d89b2cc4eb3f83ed469e9486714891587a33
lokacraft <lokacraft.id@gmail.com>
2024-11-08 02:38:26 +0700

    troubleshoot2

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone();
-  const hostname = req.headers.get("host");
+  const hostname = req.headers.get('host')!;
+  const subdomain = hostname.match(/^([^.]+)\./)?.[1];
 
-  console.log("Hostname:", hostname); // Log untuk debug
-
-  if (hostname === "admin.arthaloka.tech") {
-    url.pathname = `/admin${url.pathname}`;
-    return NextResponse.rewrite(url);
+  switch (true) {
+    case subdomain?.startsWith('admin'):
+      // Arahkan ke direktori /admin untuk subdomain admin
+      return NextResponse.rewrite(new URL(`/admin${req.nextUrl.pathname}`, req.url));
+    case subdomain?.startsWith('app'):
+      return NextResponse.rewrite(new URL(`/app${req.nextUrl.pathname}`, req.url));
+    // Tambahkan kasus lain sesuai kebutuhan
+    default:
+      // Tangani untuk domain utama jika dibutuhkan
+      return NextResponse.next();
   }
 }
 
+// Menentukan agar middleware ini hanya berjalan pada rute tertentu
 export const config = {
-  matcher: "/:path*",
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
 };
